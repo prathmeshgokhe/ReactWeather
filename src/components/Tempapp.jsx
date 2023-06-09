27e5ae2304ad6dc3fb93b314f8d4b1768309c82b
Prathmesh Gokhe <pkgokhe1969@gmail.com>
2023-06-09 16:28:34 +0530

    Weather

---
+++ src/components/Tempapp.jsx
@@ -0,0 +1,28 @@
+import React, { useState } from 'react'
+import './Styles/tempapp.scss'
+
+
+const Tempapp = () => {
+
+    const [city, setCity] = useState("Pune");
+
+    return (
+        <div className='container'>
+            <div className='main'>
+                <div className='inputData'>
+                    <input type='search' className='inputField' onChange={(event) => { }} />
+                </div>
+                <div className='info'>
+                    <h2 className='location'>
+                        <i class="fa-solid fa-street-view"></i>{city}
+                    </h2>
+                    <h1 className='temp'>5.25°Cel</h1>
+                    <h3 className='tempmin_max'> Min: 5.25°Cel | Max: 5.25°Cel</h3>
+                </div>
+            </div>
+
+        </div>
+    )
+}
+
+export default Tempapp
