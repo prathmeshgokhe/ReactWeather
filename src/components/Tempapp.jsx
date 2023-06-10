d9289e5adefdfe1939900fed2cde5c2a8226d189
Prathmesh Gokhe <pkgokhe1969@gmail.com>
2023-06-10 18:31:44 +0530

    basic updation

--- src/components/Tempapp.jsx
+++ src/components/Tempapp.jsx
@@ -1,10 +1,23 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import './Styles/tempapp.scss'
-
 
 const Tempapp = () => {
 
-    // const [city, setCity] = useState("Pune");
+    const [city, setCity] = useState(null);
+    const [search, setSearch] = useState('Nagpur')
+
+    useEffect(() => {
+
+        const fetchApi = async () => {
+            const url = `https://api.openweathermap.org/data/2.5/weather?q=${search}&appid=11054c1f7f371fe1b1ec3bf06e94e316`
+            const response = await fetch(url)
+            const resJson = await response.json();
+            console.log(resJson)
+
+        }
+
+        fetchApi();
+    })
 
     return (
         <div className='container'>
@@ -14,7 +27,7 @@ const Tempapp = () => {
                 </div>
                 <div className='info'>
                     <h2 className='location'>
-                        <i class="fa-solid fa-street-view"></i>{city}
+                        <i class="fa-solid fa-temperature-three-quarters" style="color: #ffffff;"></i>{city}
                     </h2>
                     <h1 className='temp'>5.25°Cel</h1>
                     <h3 className='tempmin_max'> Min: 5.25°Cel | Max: 5.25°Cel</h3>
